482e26b09672a431fc837e29cc12c4a64a9426cf
supermrutah <ren.miao@path.utah.edu>
2016-12-11 17:41:02 -0700

    optimize regression algorithm

--- FormatConverter.py
+++ FormatConverter.py
@@ -1,13 +1,13 @@
 import pandas as pd
 
-inputFile = 'Data/submit_0.csv'
-outputFile = 'Data/submit_1.csv'
+inputFile = 'Data/presubmit_0.csv'
+outputFile = 'Data/submit_2.csv'
 
 inputData = pd.read_csv(inputFile)
-inputData = inputData.drop('Unnamed: 0', 1)
+# inputData = inputData.drop('Unnamed: 0', 1)
 inputData.insert(0, 'id', 0)
 
 inputData['id'] = inputData['store_nbr'].map(str) + '_' + inputData['item_nbr'].map(str) + '_' + inputData['date']
 
-print(inputData)
+# print(inputData)
 inputData.to_csv(outputFile, index=False, columns=['id', 'units'])
--- linear2.py
+++ linear2.py
@@ -1,20 +1,23 @@
 import pandas as pd
 from sklearn import linear_model
 
-testFile = 'Data/testProcessed_2.csv'
-trainFile = 'Data/finalTrain_0.csv'
-outputFile = 'Data/submit_0.csv'
+testFile = 'Data/testProcessed_3.csv'
+trainFile = 'Data/finalTrain_1.csv'
+outputFile = 'Data/submit_ridge_0.5.csv'
 
 testData = pd.read_csv(testFile)
 trainData = pd.read_csv(trainFile)
-trainData = trainData.drop('Unnamed: 0', 1)
-trainData = trainData.drop('Unnamed: 0.1', 1)
+# trainData = trainData.drop('Unnamed: 0', 1)
+# trainData = trainData.drop('Unnamed: 0.1', 1)
 
-testData = testData.drop('Unnamed: 0', 1)
-testData = testData.drop('Unnamed: 0.1', 1)
+# testData = testData.drop('Unnamed: 0', 1)
+# testData = testData.drop('Unnamed: 0.1', 1)
 
 # print(testData)
 dict = {}
+feature_cols = ['temp depart', 'isWeekend', 'isHoliday', 'SnowFall', 'PrecipTotal']
+lable_col = ['units']
+
 for index, row in testData.iterrows():
     # if sno == 35:
     #     continue
@@ -25,21 +28,20 @@ for index, row in testData.iterrows():
     itemNum = row['item_nbr']
     tuple = (storeNum, itemNum)
     if tuple in dict:
-        trainRows = dict.get(tuple)
+        trainModel = dict.get(tuple)
     else:
         trainRows = trainData[(trainData['store_nbr'] == storeNum) & (trainData['item_nbr'] == itemNum)]
-        dict[tuple] = trainRows
+        x = trainRows[feature_cols]
+        y = trainRows[lable_col]
+        linreg = linear_model.Ridge(alpha=0.5)
+        linreg.fit(x, y)
+        trainModel = linreg
+        dict[tuple] = trainModel
     # print(trainRows)
 
-    linreg = linear_model.Lasso(alpha=0.1)
-    feature_cols = ['temp depart', 'isWeekend', 'isHoliday']
-    lable_col = ['units']
-    x = trainRows[feature_cols]
-    y = trainRows[lable_col]
-    linreg.fit(x, y)
     x_test = pd.DataFrame(row[feature_cols]).transpose()
     # try:
-    y_pred = linreg.predict(x_test)
+    y_pred = trainModel.predict(x_test)
     # except Exception,ex:
     #   print sno,ino
     # df_test['log1p'] = y_pred
@@ -47,5 +49,10 @@ for index, row in testData.iterrows():
     # df_test.to_csv('model/result/df_result_'+str(sno)+'_'+str(ino)+'.csv')
     testData.set_value(index, 'units', y_pred)
 
-print(testData)
-testData.to_csv(outputFile)
+# print(testData)
+# testData.to_csv(outputFile)
+testData.insert(0, 'id', 0)
+
+testData['id'] = testData['store_nbr'].map(str) + '_' + testData['item_nbr'].map(str) + '_' + testData['date']
+
+testData.to_csv(outputFile, index=False, columns=['id', 'units'])
